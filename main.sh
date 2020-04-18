8b28ee112cfa895520a4b29bb667062fc8edcd2c
Ray Kao <ray.kao@microsoft.com>
2020-04-18 19:31:51 +0000

    preparing main.sh for necessary commands

--- main.sh
+++ main.sh
@@ -1,11 +1,22 @@
 #! /bin/bash
 
-## VARIABLES FILE
+## Required Bootstrap VARIABLES
 export AZURE_SUBSCRIPTION_ID=
 export UPDATE_TO_KUBERNETES_VERSION="1.15.19"
 export K8S_CONTROL_PLANE_UPGRADE_ONLY=false
 export IN_PLACE_NODE_UPDATE=false
 
-## Load Helpers
-source ./helperFunctions.sh
+## Load helper functions
+source ./001-helperFunctions.sh
+## Load Azure CLI pre-flight Operation functions
+source ./010-azCLIOperations.sh
+## Load Cluster Level Operation functions
+source ./002-clusterOperations
+## Load nodePool Level Operation functions
+source ./003-nodePoolOperations
+## Load node Level Operation functions
+source ./004-nodeOperations
+
+# Set Azure CLI to right Subscription
+setSubscription $AZURE_SUBSCRIPTION_ID
 
