de2b04839e5d6856006724c068ec4f14683cfb4f
Ray Kao <ray.kao@microsoft.com>
2020-04-19 16:24:05 +0000

    drain and taint functions now will check based on list file

--- 030-nodePoolOperations.sh
+++ 030-nodePoolOperations.sh
@@ -4,22 +4,59 @@
 source "./040-nodeOperations.sh"
 
 ### Node Pool functions
-function createNodePoolUpgradeCandidatesJSON(){
-    local __fileName="nodePoolUpgradeCandidates.json"
 
-    echo "Generating list of AKS Node Pools to upgrade..."
+#### MARK FOR DEATH - REDUNDANT FUNCTION - createClusterUpgradeCandidatesJSON() does same thing
+# function createNodePoolUpgradeCandidatesJSON(){
+#     local __fileName="nodePoolUpgradeCandidates.json"
 
-        local $__json=$(az aks list --query "[?agentPoolProfiles[?orchestratorVersion < '$UPDATE_TO_KUBERNETES_VERSION' && osType == 'Linux']].{name: name, resourceGroup: resourceGroup, kubernetesVersion: kubernetesVersion, agentPoolProfiles: agentPoolProfiles[].{name: name, count: count, vmSize: vmSize, orchestratorVersion: orchestratorVersion}}" -o json)
+#     echo "Generating list of AKS Node Pools to upgrade..."
+
+#         local $__json=$(az aks list --query "[?agentPoolProfiles[?orchestratorVersion < '$UPDATE_TO_KUBERNETES_VERSION' && osType == 'Linux']].{name: name, resourceGroup: resourceGroup, kubernetesVersion: kubernetesVersion, agentPoolProfiles: agentPoolProfiles[].{name: name, count: count, vmSize: vmSize, orchestratorVersion: orchestratorVersion}}" -o json)
 
 
-    if [ $? -eq 0 ]
-    then
-        echo "Succeeded to create list of Node Pool upgrade candidates"
-        echo $__json | tee .tmp/$__fileName
-    else
-        echo "Failed to create list of Node Pool upgrade candidates" > err.log 
-        return 1
-    fi
+#     if [ $? -eq 0 ]
+#     then
+#         echo "Succeeded to create list of Node Pool upgrade candidates"
+#         echo $__json | tee .tmp/$__fileName
+#     else
+#         echo "Failed to create list of Node Pool upgrade candidates" > err.log 
+#         return 1
+#     fi
+# }
+####
+
+
+function upgradeNodePools() {
+    local __RG=$1
+    local __clusterName=$2
+    local __oldNodePoolNames=$3
+    
+    for __oldNodePoolName in __nodePoolNames
+    do
+        local __nodePoolCount=$(cat .tmp/$CLUSTER_FILE_NAME | jq -r '.[] | select(.name=="default-demo-corp-pubaks").agentPoolProfiles[] | select(.name=="default").count')
+        local __nodePoolVMSize=$(cat .tmp/$CLUSTER_FILE_NAME | jq -r '.[] | select(.name=="default-demo-corp-pubaks").agentPoolProfiles[] | select(.name=="default").vmSize')
+        
+        upgradeNodePool $__RG $__clusterName $__oldNodePoolName $__nodePoolCount $__nodePoolVMSize
+    done
+}
+
+function upgradeNodePool() {
+    local __RG=$1
+    local __clusterName=$2
+    local __oldNodePoolName=$3
+    local __newNodePoolName=$__oldNodePoolName$UPDATE_TO_KUBERNETES_VERSION_SAFE_STRING
+    local __nodePoolCount=$4
+    local __nodePoolVMSize=$5
+
+    checkNodePoolNameisValid $__RG $__clusterName $__oldNodePoolName
+    createNewNodePool $__RG $__clusterName $__newNodePoolName $__nodePoolCount $__nodePoolVMSize
+}
+
+function createListOfNodesInNodePool() {
+    local __nodePoolName=$1
+
+    kubectl get nodes | grep -i $__nodePoolName | awk '{print $1}' > .tmp/$__nodePoolName.txt
+    return 0
 }
 
 function checkNodePoolNameisValid() {
@@ -59,35 +96,50 @@ function createNewNodePool() {
     echo "done - Creating new NodePool"
 }
 
-function listNodesInNodePool() {
-    local __nodePoolName=$1
-
-    # Find current NodePool nodes and create file
-    echo $(kubectl get nodes | grep -i $__nodePoolName | awk '{print $1}') | tee listNodesinNodePool-$__nodePoolName.txt
-    return 0
-}
-
 # Taint Node Pool
 function tainitNodePool() {
-    while NODES= read -r NODE
-    do
-        taintNode $__nodeName
-    done < nodes.txt
+    local __nodePoolName=$1
+    local __nodesListFile="./tmp/$__nodePoolName.txt"
+    local __taintListFile=="./tmp/$__nodePoolName-taint.txt"
 
-    echo "done - Tainting current NodePool"
+    # duplicate Node List to Taint List to track progress
+    cp $__nodesListFile $__taintListFile
+
+    for __nodeName in $(cat $__taintListFile)
+    do
+        echo "Tainting Node '$__nodeName' in Node Pool '$__nodePoolName'"
+        
+        kubectl taint node $__nodeName GettingUpgraded=:NoSchedule
+        
+        # remove node name from list to track progress
+        sed -e s/$__nodeName//g -i .$__taintListFile
+    done
+
+    echo "done - Tainting current Node Pool '$__nodePoolName'"
+    rm $__taintListFile
 }
 
 function drainNodePool() {
     local __nodePoolName=$1
-
-    echo "Draining current NodePool"
-
-    while NODES= read -r NODE
-    do
-      drainNode $__nodeName  
-    done < nodes.txt
+    local __nodesListFile="./tmp/$__nodePoolName.txt"
+    local __drainListFile=="./tmp/$__nodePoolName-drain.txt"
     
-    echo "done - Draining current NodePool"
+    # duplicate Node List to Taint List to track progress
+    cp $__nodesListFile $__drainListFile
+
+    for __nodeName in $(cat $__drainListFile)
+    do
+        echo "Draining Node '$__nodeName' in Node Pool '$__nodePoolName'"
+        
+        kubectl drain $NODE --ignore-daemonsets --delete-local-data
+        sleep 60
+        
+        # remove node name from list to track progress
+        sed -e s/$__nodeName//g -i $__drainListFile
+    done
+
+    echo "done - Draining current Node Pool '$__nodePoolName'"
+    rm $__drainListFile
 }
 
 function deleteNodePool() {
@@ -110,30 +162,4 @@ function deleteNodePool() {
     else
         echo "Failure: Unable to delete Node Pool: $__nodePoolName" > err.log
     fi
-}
-
-function upgradeNodePools() {
-    local __RG=$1
-    local __clusterName=$2
-    local __oldNodePoolNames=$3
-    
-    for __oldNodePoolName in __nodePoolNames
-    do
-        local __nodePoolCount=$(cat .tmp/$CLUSTER_FILE_NAME | jq -r '.[] | select(.name=="default-demo-corp-pubaks").agentPoolProfiles[] | select(.name=="default").count')
-        local __nodePoolVMSize=$(cat .tmp/$CLUSTER_FILE_NAME | jq -r '.[] | select(.name=="default-demo-corp-pubaks").agentPoolProfiles[] | select(.name=="default").vmSize')
-        
-        upgradeNodePool $__RG $__clusterName $__oldNodePoolName $__nodePoolCount $__nodePoolVMSize
-    done
-}
-
-function upgradeNodePool() {
-    local __RG=$1
-    local __clusterName=$2
-    local __oldNodePoolName=$3
-    local __newNodePoolName=$__oldNodePoolName$UPDATE_TO_KUBERNETES_VERSION_SAFE_STRING
-    local __nodePoolCount=$4
-    local __nodePoolVMSize=$5
-
-    checkNodePoolNameisValid $__RG $__clusterName $__oldNodePoolName
-    createNewNodePool $__RG $__clusterName $__newNodePoolName $__nodePoolCount $__nodePoolVMSize
 }
