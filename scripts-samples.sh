398aeb5b7438c6d9930cf7bbb830c13f4a24d308
Ray Kao <ray.kao@microsoft.com>
2020-04-20 22:34:45 +0000

    removing old files

--- scripts-samples.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Sample for nodepoolupgrade
-# K8s Version, Resource Group, Cluster Name, Current Node Pool, New Node Pool
-./nodepoolupgrade.sh '1.16.7' 'khaks-rg' 'khaks20200420' 'nodepool2' 'nodepool3'
